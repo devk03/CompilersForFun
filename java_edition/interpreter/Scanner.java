d0a650a4988b4cd225693b805b6dae2c802c81f8
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-12-25 03:23:59 -0500

    Added scanning functionality for single character lexemes.

--- java_edition/interpreter/Scanner.java
+++ java_edition/interpreter/Scanner.java
@@ -1,9 +1,11 @@
 package interpreter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import interpreter.Token;
+
+import static interpreter.TokenType.*;
 
 public class Scanner {
     private String source; // The raw source code casted as a string
@@ -12,10 +14,56 @@ public class Scanner {
     private int current; // Current index being scanned
     private int line; // Current line in the source code for errors
 
-    Scanner(String source){
+    Scanner(String source) {
         this.source = source;
         this.tokens = new ArrayList<Token>();
-        this.start = 0; 
+        this.start = 0;
         this.current = 0;
+        this.line = 1;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advanceToken();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                System.out.println("Unexpected Lexeme:" + c);
+                break;
+            }
+    }
+
+    private char advanceToken(){
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
